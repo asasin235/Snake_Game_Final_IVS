4d06cb66a3d22cc3fd5b515014943d5393cfa752
asasin235 <aatif187899@st.jmi.ac.in>
2021-09-12 13:47:52 +0530

    Game Commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import Snake from './Snake/Snake'
+import  Navbar  from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container'
+import ReactBootstrap from 'react-bootstrap'
+import Nav from 'react-bootstrap/Nav'
+
+function App() {
+  
+  return (
+    < >
+     <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="./snake_logo.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+     Snake Game
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+    <div className="App">
+
+       <Snake 
+        snakeColor="#248ec2"
+        fruitColor="#1d355e"
+        backgroundColor="#ebebeb"
+        />
+    </div>
+    <div className="fixed-bottom">  
+            <Navbar color="dark" dark>
+                <Container>
+                    <Navbar.Brand>Made using React and bootstrap by Aatif Rashid</Navbar.Brand>
+                </Container>
+            </Navbar>
+        </div>
+    </>
+    
+  );
+}
+
+export default App;
